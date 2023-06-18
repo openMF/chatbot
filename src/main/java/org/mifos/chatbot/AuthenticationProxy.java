2c1b7ec48d0437698c6d0ed6d529757a4906e9cb
meghnabajoria <meghnabajoria8@gmail.com>
2023-06-18 13:53:49 -0700

    added API for sending user message to RASA

--- src/main/java/org/mifos/chatbot/AuthenticationProxy.java
+++ src/main/java/org/mifos/chatbot/AuthenticationProxy.java
@@ -4,6 +4,14 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+//@FeignClient(name="fineract-provider", url = "https://localhost:8443/fineract-provider/")
+//public interface AuthenticationProxy {
+//    @PostMapping(value = "/v1/authentication", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<String> authenticate(String requestBodyAsJson);
+//}
+
 
 @FeignClient(name="demo-service", url = "localhost:8082")
 public interface AuthenticationProxy {
--- src/main/java/org/mifos/chatbot/controller.java
+++ src/main/java/org/mifos/chatbot/controller.java
@@ -1,27 +1,63 @@
 package org.mifos.chatbot;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
 
 
 @RestController
 @RequestMapping(path = "/")
 public class controller {
 
+    String rasaEndpoint = "http://localhost:5005/webhooks/rest/webhook";
+
     @Autowired
     AuthenticationProxy authenticationProxy;
-    @GetMapping(value = "/qbpost", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> test() {
-        return new ResponseEntity<>("Service is running.", HttpStatus.OK);
-    }
 
-    @GetMapping("/feignclient")
+    @GetMapping("/authenticate-user")
     public  ResponseEntity<String> your_client_controller_api() {
         return authenticationProxy.test();
+    }
+
+    @PostMapping("/chat")
+    public String chat(@RequestParam String message) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create(mediaType, createJSONRequest(message));
+        Request request = new Request.Builder()
+                .url(rasaEndpoint)
+                .method("POST", body)
+                .addHeader("Content-Type", "text/plain")
+                .build();
+        Response response = client.newCall(request).execute();
+        if(response.code() == 200) {
+            String responseBody = response.body().string();
+            return responseBody;
+        }
+        else {
+            return "Request failed with status code: " + response.code();
+        }
+    }
+
+    private String createJSONRequest(String string) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode jsonNode = mapper.createObjectNode();
+
+        jsonNode.put("message", string);
+
+        return mapper.writeValueAsString(jsonNode);
     }
 }
