66e037dba71a11406a12c65524abc30c1b55e636
Meghna Bajoria <meghnabajoria8@gmailcom>
2023-07-31 16:06:34 -0700

    added timeline.java

---
+++ server/src/main/java/org/mifos/chatbot/server/config/openFeign/Response/Timeline.java
@@ -0,0 +1,18 @@
+package org.mifos.chatbot.server.config.openFeign.Response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Timeline {
+    private List<Integer> expectedMaturityDate;
+}
